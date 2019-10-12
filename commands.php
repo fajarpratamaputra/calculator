069e0b619a0ebc68809f3d782ee938ae245986b5
fajarputra_kareba <fajarpratamap@gmail.com>
2019-10-12 09:04:30 +0700

    v0.3

--- commands.php
+++ commands.php
@@ -4,4 +4,5 @@ return [
     // TODO : Add list of commands here
     \Jakmall\Recruitment\Calculator\Commands\Add::class,
     \Jakmall\Recruitment\Calculator\Commands\Substract::class,
+    \Jakmall\Recruitment\Calculator\Commands\Multiply::class,
 ];
